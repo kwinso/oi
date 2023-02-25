97a4b16c00add2cd83ef4ff07cd01f75a33d14de
kwinso <ruslan.sot70@gmail.com>
2023-02-25 17:40:32 +0300

    Add a banner.

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fatih/color"
 	"oilang/internal/repl"
 	"os"
 )
 
+const BANNER = `░░░░░░░█████╗░██╗░░░░░░░█████╗░██╗░░░░░░
+░░░░░░██╔══██╗██║░░░░░░██╔══██╗██║░░░░░░
+░░░░░░██║░░██║██║█████╗██║░░██║██║░░░░░░
+░░░░░░██║░░██║██║╚════╝██║░░██║██║░░░░░░
+██╗██╗╚█████╔╝██║░░░░░░╚█████╔╝██║██╗██╗
+╚═╝╚═╝░╚════╝░╚═╝░░░░░░░╚════╝░╚═╝╚═╝╚═╝
+███████╗██████╗░░██╗░░░░░░░██╗██╗███╗░░██╗
+██╔════╝██╔══██╗░██║░░██╗░░██║██║████╗░██║
+█████╗░░██████╔╝░╚██╗████╗██╔╝██║██╔██╗██║
+██╔══╝░░██╔══██╗░░████╔═████║░██║██║╚████║
+███████╗██║░░██║░░╚██╔╝░╚██╔╝░██║██║░╚███║
+╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝`
+
 func main() {
-	color.Magenta("Welcome to the REPL of oi language. Feel free to play around!")
+	color.Magenta(BANNER)
+	color.White("Welcome to the REPL of oi language.\nFeel free to play around!")
+	fmt.Println()
 
 	repl.Start(os.Stdin, os.Stdout)
 }
